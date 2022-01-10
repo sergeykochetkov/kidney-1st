eef75972ae4b13ee1c336d4c302751f52f9a83dd
sergeykochetkov <sergeykochetkov@yandex.ru>
2022-01-10 08:58:48 +0300

    dice_per_batch_impl

--- src/losses.py
+++ src/losses.py
@@ -3,18 +3,14 @@ from lovasz_loss import lovasz_hinge
 
 
 def dice_loss(logits, target):
-    smooth = 1.
+    smooth = 1e-3
     prob  = torch.sigmoid(logits)
-    batch = prob.size(0)
-    
-    prob   = prob.view(batch,1,-1)
-    target = target.view(batch,1,-1)
-    
-    intersection = torch.sum(prob*target, dim=2)
-    denominator  = torch.sum(prob, dim=2) + torch.sum(target, dim=2)
+
+
+    intersection = torch.sum(prob*target)
+    denominator  = torch.sum(prob) + torch.sum(target)
     
     dice = (2*intersection + smooth) / (denominator + smooth)
-    dice = torch.mean(dice)
     dice_loss = 1. - dice
     return dice_loss
 
